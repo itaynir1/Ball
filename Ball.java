c8698563876a7ea8d76f4576b91a6def5d0af3c6
itaynir1 <78868632+itaynir1@users.noreply.github.com>
2021-08-13 18:22:42 +0300

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,24 @@
+/*
+*Author - Itay Nir
+* 208988725
+* this program calculate the area and the cubage of ball.
+ */
+
+import java.util.Scanner;
+public class Ball
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int radius;
+        System.out.println("you should enter radius "+" and this program calculate area and volume of ball");
+        radius = scan.nextInt();
+        final double PI= 3.14;
+        double volume=(4*PI*Math.pow(radius, 3 ))/3; //formula for cubage ball.
+        double area=4*PI*Math.pow(radius, 2);// formula for surface of ball.
+
+        System.out.println(" the area of ball is " + area );
+        System.out.println("and the volume of ball is"+ volume);
+
+    }
+}
